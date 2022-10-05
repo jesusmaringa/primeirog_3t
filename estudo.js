916045d91ec3350f9ef51cd466c91c97c320d135
Jesus <101044903+jesusmaringa@users.noreply.github.com>
2022-10-05 11:56:17 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -12,8 +12,24 @@ function escreva(){
     document.write(tabuada+" x 8 = "+(tabuada*8)+"<br>");
     document.write(tabuada+" x 9 = "+(tabuada*9)+"<br>");
     document.write(tabuada+" x 10 = "+(tabuada*10)+"<br>");
-    
+}
 
+let lista = ["Terra","Marte","Saturno","Jupiter","Vênus","Mercurio"];
+
+function mostraLista(){
+    document.write("Tamanho da lista: " + lista.length + "<br>");
+    for(let i = 0; i < lista.length; i++){
+        document.write("Planela " + lista[i] + "<br>");
+    }
+}
+
+function repete(){
+    for(let i = 1; i <= 10; i++){
+        document.write("Tabuada do " + i + "<br>");
+        for(let j = 1; j <=10; j++){
+            document.write(i+" x " + j + " = "+(i*j)+"<br>");
+        }
+    }
 }
 
 
