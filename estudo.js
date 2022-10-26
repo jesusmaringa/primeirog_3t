8d40458a7b83178160bd8552e7a0c8e715f37452
Jesus <101044903+jesusmaringa@users.noreply.github.com>
2022-10-26 11:54:57 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -2,6 +2,24 @@ function total(){
     let c = document.getElementById("capital").value;
     let j = document.getElementById("juros").value;
     let t = document.getElementById("meses").value;
+    if(!Number(c)){
+        alert("O valor do capital deve ser um número.");
+        document.getElementById("capital").value = "";
+        document.getElementById("capital").focus();
+        return 
+    }
+    if(!Number(j)){
+        alert("O valor dos juros deve ser um número.");
+        document.getElementById("juros").value = "";
+        document.getElementById("juros").focus();
+        return 
+    }
+    if(!Number(t)){
+        alert("A quantidade de meses deve ser um número.");
+        document.getElementById("meses").value = "";
+        document.getElementById("meses").focus();
+        return 
+    }
     let m = 0;
     for(let i = 1; i <= t; i++){
         m = c * (1 + (j/100));
@@ -10,3 +28,4 @@ function total(){
     }
     document.write("Montante: " + m);
 }
+
